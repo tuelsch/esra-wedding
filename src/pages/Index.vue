25a4df77b3b54d89b8faac613fe15f877723f586
Philipp Gfeller <tuelsch@gmail.com>
2020-01-06 16:04:03 +0100

    s

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,7 +4,7 @@
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
     <g-image alt="Example image" src="~/favicon.png" width="135" />
 
-    <h1>Hello, world!</h1>
+    <h1>{{Home.title}}</h1>
 
     <p>
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
@@ -19,7 +19,11 @@
 </template>
 
 <script>
+import Home from '@/admin/content/home.yml';
 export default {
+  computed: {
+    Home: () => Home,
+  },
   metaInfo: {
     title: 'Hello, world!'
   }
