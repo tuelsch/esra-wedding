dd3841fcd1f29d5cfc194ebd52b9f77f4b214213
Philipp Gfeller <tuelsch@gmail.com>
2020-01-08 14:37:55 +0100

    fix local storage issue

--- src/router.js
+++ src/router.js
@@ -1,4 +1,5 @@
 export default function (router) {
+  if (!localStorage) return;
   router.beforeEach((to, from , next) => {
     const pw = localStorage.getItem('pw');
     if (pw === null && to.path !== '/') {
