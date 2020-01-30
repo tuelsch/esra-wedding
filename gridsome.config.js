79549e23b9866247798492489cb16eeb2d1f2fad
Philipp Gfeller <tuelsch@gmail.com>
2020-01-30 11:00:07 +0100

    fix cms

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,20 +1,21 @@
-// This is where project configuration and plugin options are located. 
+// This is where project configuration and plugin options are located.
 // Learn more: https://gridsome.org/docs/config
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'esra-wedding',
+  siteName: "esra-wedding",
   transformers: {
     remark: {
-      externalLinksTarget: '_blank',
-      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
-      anchorClassName: 'icon icon-link',
+      externalLinksTarget: "_blank",
+      externalLinksRel: ["nofollow", "noopener", "noreferrer"],
+      anchorClassName: "icon icon-link",
       plugins: [
         // ...global plugins
       ]
-    }
+    },
+    yaml: {}
   },
   plugins: [
     {
@@ -24,7 +25,15 @@ module.exports = {
       }
     },
     {
-      use: 'gridsome-plugin-netlify-cms-paths'
+      use: "gridsome-plugin-netlify-cms-paths"
+    },
+    {
+      use: "@gridsome/source-filesystem",
+      options: {
+        path: "src/admin/content/**/*.yml",
+        typeName: "netlifyPages",
+        yaml: {}
+      }
     }
   ]
-}
+};
