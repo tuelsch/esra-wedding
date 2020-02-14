354b27ba43d0f6aaa0ee26296b13ac6abb0ebf21
= <=>
2020-02-14 19:07:06 +0100

    FIrebase functions

---
+++ functions/src/index.ts
@@ -0,0 +1,67 @@
+// Copyright 2017 Google LLC.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import * as functions from "firebase-functions";
+import * as _ from "lodash";
+import { google } from "googleapis";
+const sheets = google.sheets("v4");
+
+const spreadsheetId = "1pQ7hh9OKpAy6elcZePu2yQl1UNsk4aRk_FW83ZPRi44";
+
+const serviceAccount = require("../serviceAccount.json");
+
+const jwtClient = new google.auth.JWT({
+  email: serviceAccount.client_email,
+  key: serviceAccount.private_key,
+  scopes: ["https://www.googleapis.com/auth/spreadsheets"] // read and write sheets
+});
+const jwtAuthPromise = jwtClient.authorize();
+
+type Apero = {
+  adults: number;
+  children: number;
+  comments: string;
+  created: number;
+  firstname: string;
+  lastname: string;
+};
+
+export const copyScoresToSheet = functions.database
+  .ref("/apero")
+  .onUpdate(async change => {
+    const data: Apero = change.after.val();
+
+    await jwtAuthPromise;
+    await sheets.spreadsheets.values.append(
+      {
+        auth: jwtClient,
+        spreadsheetId: spreadsheetId,
+        range: "Apero!A1:F1", // update this range of cells
+        valueInputOption: "RAW",
+        requestBody: {
+          values: [
+            [
+              data.firstname,
+              data.lastname,
+              data.adults,
+              data.children,
+              data.comments,
+              data.created
+            ]
+          ]
+        }
+      },
+      {}
+    );
+  });
